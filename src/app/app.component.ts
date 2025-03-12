888b864f16c550aec6725f621e6e8951e61082c6
samuel135679 <samuelcaccin17@gmail.com>
2025-03-12 12:01:56 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet,RouterLink],
 })
 export class AppComponent {
   constructor() {}
